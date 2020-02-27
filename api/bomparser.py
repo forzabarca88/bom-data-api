fc0d7ecc0967649d1cc09ca814165d79da61b70a
Justin D'Cruze <forzabarca88@gmail.com>
2020-02-27 11:33:17 +1100

    wip refactoring

---
+++ api/bomparser.py
@@ -0,0 +1,9 @@
+from bs4 import BeautifulSoup
+
+class BomParser(object):
+    '''
+    base class for parsing BOM pages
+    '''
+
+    def __init__(self):
+        pass
---
+++ api/bomrequest.py
@@ -0,0 +1,67 @@
+from bs4 import BeautifulSoup
+import requests
+
+
+class BomRequest(object):
+    '''
+    Class used for managing requests for data from BOM
+    '''
+
+    def __init__(self, bom_urls):
+        self.bom_urls = bom_urls
+
+
+class BomUrls(object):
+    '''
+    Class for managing the various URLs for BOM data
+    '''
+
+    def __init__(self):
+        self._base_url = 'http://www.bom.gov.au'
+        self._site_map_url = '/site-map'
+        self._site_map_data = self._get_site_map_data()
+        self._site_map_links = self._site_map_data.find_all('a')
+        self._state_map = {
+            'vic': {
+                'title': 'Latest Weather Observations for Victoria',
+                'url': None
+                },
+            'nsw': {
+                'title': 'Latest Weather Observations for New South Wales',
+                'url': None
+                },
+            'wa': {
+                'title': 'Latest Weather Observations for Western Australia',
+                'url': None
+                },
+            'nt': {
+                'title': 'Latest Weather Observations for Northern Territory',
+                'url': None
+                },
+            'sa': {
+                'title': 'Latest Weather Observations for South Australia',
+                'url': None
+                },
+            'act': {
+                'title': 'Latest Weather Observations for the Canberra Area',
+                'url': None
+                },
+            'qld': {
+                'title': 'Latest Weather Observations for Queensland',
+                'url': None
+                }
+        }
+        self._get_summary_observations()
+
+    def _get_site_map_data(self):
+        data = requests.get('{}{}'.format(self._base_url, self._site_map_url)).text
+        return BeautifulSoup(data, 'lxml')
+
+    def _get_summary_observations(self):
+        for state in self._state_map:
+            for link in self._site_map_links:
+                if link.get('title', '').lower().find(
+                        self._state_map[state]['title'].lower()) == 0:
+                    self._state_map[state]['url'] = link.get('href')
+                    break
+        
